0cd4d843577e0099309330bcf9d274e94aa46d16
Omojolade <omolade.omololu@decagon.dev>
2020-12-21 00:01:14 +0100

    Create Search and Spinner components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
-import React, {Component} from "react"
-import './App.css';
-import Navbar from "./components/layout/Navbar"
-import UserItem from "./components/layout/users/UserItem";
-
-
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/layout/users/Users";
+import Search from "./components/layout/users/Users";
+import axios from "axios";
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
 
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get(`https://api.github.com/users?client_id=$
+    {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
+    {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
+    this.setState({ users: res.data, loading: false });
+  }
   render() {
     return (
       <div className="App">
-       <Navbar icon="fab fa-github" title="Github Finder"/>
-       <UserItem/>
+        <Navbar icon="fab fa-github" title="Github Finder" />
+        <div className="container">
+          <Search />
+        </div>
+   
+        <div className="container">
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
       </div>
     );
-    }
- 
+  }
 }
 
 export default App;
